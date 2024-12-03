5faa9d04ab9655cc50782ba69263ea7795a60a2f
Benjamin Bachmann <beba@Benjamins-MacBook-Pro.local>
2024-12-03 08:19:37 +0100

    Refactor regex solution

--- src/s_03.rs
+++ src/s_03.rs
@@ -13,12 +13,14 @@ lazy_static! {
     static ref DONT: Regex = Regex::new(r"don't\(\)").unwrap();
 }
 
+#[derive(Clone)]
 enum OperationType {
     Mul(i32, i32),
     Do,
     Dont,
 }
 
+#[derive(Clone)]
 struct Operation {
     position: usize,
     operation: OperationType,
@@ -42,73 +44,68 @@ impl Solution for S {
     }
 
     fn solve_one(&self, input: &PuzzleInput) -> Option<String> {
-        let mut result = 0;
+        let operations = parse_operations(input);
 
-        for line in input.lines.iter() {
-            if MUL.is_match(line) {
-                let caps = MUL.captures_iter(line);
-                for cap in caps {
-                    //println!("{:?}", cap);
-                    let num = cap.get(1).unwrap().as_str().parse::<i32>().unwrap();
-                    let num2 = cap.get(2).unwrap().as_str().parse::<i32>().unwrap();
-                    result += num * num2;
-                }
-            }
-        }
+        let result: i32 = operations
+            .iter()
+            .map(|op| match op.operation {
+                OperationType::Mul(a, b) => a * b,
+                _ => 0,
+            })
+            .sum();
 
         Some(format!("{}", result))
     }
 
     fn solve_two(&self, input: &PuzzleInput) -> Option<String> {
+        let operations = parse_operations(input);
+
         let mut result = 0;
         let mut enabled = true;
 
-        for line in input.lines.iter() {
-            let mut operations = vec![];
-
-            let muls = MUL.captures_iter(line);
-            for mul in muls {
-                let num = mul.get(1).unwrap().as_str().parse::<i32>().unwrap();
-                let num2 = mul.get(2).unwrap().as_str().parse::<i32>().unwrap();
-                operations.push(Operation {
-                    position: mul.get(0).unwrap().start(),
-                    operation: OperationType::Mul(num, num2),
-                });
-            }
-            let dos = DO.captures_iter(line);
-            for d in dos {
-                operations.push(Operation {
-                    position: d.get(0).unwrap().start(),
-                    operation: OperationType::Do,
-                });
-            }
-            let donts = DONT.captures_iter(line);
-            for dont in donts {
-                operations.push(Operation {
-                    position: dont.get(0).unwrap().start(),
-                    operation: OperationType::Dont,
-                });
-            }
-
-            operations.sort_by(|a, b| a.position.cmp(&b.position));
-           
-            for op in operations {
-                match op.operation {
-                    OperationType::Mul(num, num2) => {
-                        if enabled {
-                            result += num * num2;
-                        }
-                    }
-                    OperationType::Do => {
-                        enabled = true;
-                    }
-                    OperationType::Dont => {
-                        enabled = false;
+        for op in operations {
+            match op.operation {
+                OperationType::Mul(a, b) => {
+                    if enabled {
+                        result += a * b
                     }
                 }
+                OperationType::Do => enabled = true,
+                OperationType::Dont => enabled = false,
             }
         }
 
         Some(format!("{}", result))
     }
+}
+
+fn parse_operations(input: &PuzzleInput) -> Vec<Operation> {
+    let mut operations = vec![];
+    for line in input.lines.iter() {
+        let mut line_operations = vec![];
+        for mul in MUL.captures_iter(line) {
+            let num = mul.get(1).unwrap().as_str().parse::<i32>().unwrap();
+            let num2 = mul.get(2).unwrap().as_str().parse::<i32>().unwrap();
+            line_operations.push(Operation {
+                position: mul.get(0).unwrap().start(),
+                operation: OperationType::Mul(num, num2),
+            });
+        }
+        for d in DO.captures_iter(line) {
+            line_operations.push(Operation {
+                position: d.get(0).unwrap().start(),
+                operation: OperationType::Do,
+            });
+        }
+        for dont in DONT.captures_iter(line) {
+            line_operations.push(Operation {
+                position: dont.get(0).unwrap().start(),
+                operation: OperationType::Dont,
+            });
+        }
+        line_operations.sort_by(|a, b| a.position.cmp(&b.position));
+        operations.extend_from_slice(&line_operations);
+    }
+
+    operations
 }
