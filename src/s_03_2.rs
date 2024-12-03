b2eea4d26a3abc2221fd94697b15735f890aa420
Benjamin Bachmann <beba@Benjamins-MacBook-Pro.local>
2024-12-03 11:49:43 +0100

    Uglier version of impl with nom crate.

--- src/s_03_2.rs
+++ src/s_03_2.rs
@@ -3,18 +3,22 @@ use crate::common::*;
 use nom::{
     branch::alt,
     bytes::complete::{tag, take_while_m_n},
-    character::complete::anychar,
+    character::complete::{anychar, char},
     combinator::{map, map_res},
-    multi::many0,
+    error::ParseError,
+    multi::fold_many0,
     sequence::{delimited, separated_pair},
-    IResult,
+    IResult, InputLength, Parser,
 };
+
+type R<'a, T> = IResult<&'a str, T>;
+type Context<'a> = (&'a str, (bool, u32));
 
 pub struct S {}
 
 #[derive(Clone, Debug)]
 enum OperationType {
-    Mul(i32, i32),
+    Mul(u32, u32),
     Do,
     Dont,
 }
@@ -37,73 +41,59 @@ impl Solution for S {
     }
 
     fn solve_one(&self, input: &PuzzleInput) -> Option<String> {
-        let operations = parse_operations(&input.input).unwrap().1;
-
-        let result: i32 = operations
-            .iter()
-            .map(|op| match op {
-                OperationType::Mul(a, b) => a * b,
-                _ => 0,
-            })
-            .sum();
-
+        let ops = alt((map(parse_mul, Some), consume_invalid));
+        let result = parse_and_sum(&input.input, ops);
         Some(format!("{}", result))
     }
 
     fn solve_two(&self, input: &PuzzleInput) -> Option<String> {
-        let operations = parse_operations(&input.input).unwrap().1;
-
-        let mut result = 0;
-        let mut enabled = true;
-
-        for op in operations {
-            match op {
-                OperationType::Mul(a, b) => {
-                    if enabled {
-                        result += a * b
-                    }
-                }
-                OperationType::Do => enabled = true,
-                OperationType::Dont => enabled = false,
-            }
-        }
-
+        let ops = alt((
+            map(alt((parse_mul, parse_do, parse_dont)), Some),
+            consume_invalid,
+        ));
+        let result = parse_and_sum(&input.input, ops);
         Some(format!("{}", result))
     }
 }
 
-fn parse_operations(input: &str) -> IResult<&str, Vec<OperationType>> {
-    map(
-        many0(alt((
-            map(alt((parse_mul, parse_do, parse_dont)), Some),
-            map(consume_invalid, |_| None),
-        ))),
-        |opt_vec| opt_vec.into_iter().flatten().collect(),
+fn parse_and_sum<'a>(input: &'a str, f: impl FnMut(&'a str) -> R<Option<OperationType>>) -> u32 {
+    fold_many0(
+        f,
+        || (1, 0),
+        |(enabled, result), op| match op {
+            Some(OperationType::Mul(a, b)) => (enabled, result + enabled * a * b),
+            Some(OperationType::Do) => (1, result),
+            Some(OperationType::Dont) => (0, result),
+            _ => (enabled, result),
+        },
     )(input)
+    .unwrap()
+    .1
+     .1
 }
 
-fn parse_mul(input: &str) -> IResult<&str, OperationType> {
+fn parse_mul(input: &str) -> R<OperationType> {
     let operands = delimited(
         tag("mul("),
-        separated_pair(parse_number, tag(","), parse_number),
-        tag(")"),
+        separated_pair(parse_number, char(','), parse_number),
+        char(')'),
     );
-    map(operands, |(a, b)| OperationType::Mul(a as i32, b as i32))(input)
+    map(operands, |(a, b)| OperationType::Mul(a as u32, b as u32))(input)
 }
 
-fn parse_number(input: &str) -> IResult<&str, u16> {
+fn parse_number(input: &str) -> R<u16> {
     let digits = take_while_m_n(1, 3, |c: char| c.is_ascii_digit());
     map_res(digits, |s: &str| s.parse::<u16>())(input)
 }
 
-fn parse_do(input: &str) -> IResult<&str, OperationType> {
+fn parse_do(input: &str) -> R<OperationType> {
     map(tag("do()"), |_| OperationType::Do)(input)
 }
 
-fn parse_dont(input: &str) -> IResult<&str, OperationType> {
+fn parse_dont(input: &str) -> R<OperationType> {
     map(tag("don't()"), |_| OperationType::Dont)(input)
 }
 
-fn consume_invalid(input: &str) -> IResult<&str, ()> {
-    map(anychar, |_| ())(input)
+fn consume_invalid(input: &str) -> R<Option<OperationType>> {
+    map(anychar, |_| None)(input)
 }
