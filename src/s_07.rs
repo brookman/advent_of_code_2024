3ac4b2b5a53b432fc116dbaf7a1fa4419a39f24e
Benjamin Bachmann <beba@Benjamins-MacBook-Pro.local>
2024-12-07 14:51:12 +0100

    Day 7: 6-10x speedup with parallelization

--- src/s_07.rs
+++ src/s_07.rs
@@ -1,4 +1,7 @@
+use std::iter::repeat;
+
 use itertools::Itertools;
+use rayon::prelude::*;
 
 use crate::common::*;
 
@@ -7,39 +10,7 @@ pub struct S;
 impl Solution for S {
     fn solve_one(&self, input: &PuzzleInput) -> String {
         let operators = ['+', '*'];
-        let mut result = 0;
-        for line in &input.lines {
-            let mut parts = line.split(':');
-            let test_value = parts.next().unwrap().parse::<u64>().unwrap();
-            let rem = parts
-                .next()
-                .unwrap()
-                .trim()
-                .split(' ')
-                .map(|s| s.parse::<u64>().unwrap())
-                .collect::<Vec<_>>();
-
-            for op_combinations in std::iter::repeat(operators.iter())
-                .take(rem.len() - 1)
-                .multi_cartesian_product()
-            {
-                let mut res = rem[0];
-                for (i, op) in op_combinations.iter().enumerate() {
-                    let a = rem[i + 1];
-                    match op {
-                        '+' => res += a,
-                        '*' => res *= a,
-                        _ => panic!("Unknown operator"),
-                    }
-                }
-                if res == test_value {
-                    result += test_value;
-                    break;
-                }
-            }
-        }
-
-        result.to_string()
+        solve(input, &operators).to_string()
     }
 
     fn test_input_one(&self) -> &str {
@@ -61,41 +32,7 @@ impl Solution for S {
 
     fn solve_two(&self, input: &PuzzleInput) -> String {
         let operators = ['+', '*', '|'];
-        let mut result = 0;
-        for line in &input.lines {
-            let mut parts = line.split(':');
-            let test_value = parts.next().unwrap().parse::<u64>().unwrap();
-            let rem = parts
-                .next()
-                .unwrap()
-                .trim()
-                .split(' ')
-                .map(|s| s.parse::<u64>().unwrap())
-                .collect::<Vec<_>>();
-
-            for op_combinations in std::iter::repeat(operators.iter())
-                .take(rem.len() - 1)
-                .multi_cartesian_product()
-            {
-                let mut res = rem[0];
-                for (i, op) in op_combinations.iter().enumerate() {
-                    let a = rem[i + 1];
-                    match op {
-                        '+' => res += a,
-                        '*' => res *= a,
-                        '|' => res = res * 10u64.pow(a.ilog10() + 1) + a,
-                        _ => panic!("Unknown operator"),
-                    }
-                }
-
-                if res == test_value {
-                    result += test_value;
-                    break;
-                }
-            }
-        }
-
-        result.to_string()
+        solve(input, &operators).to_string()
     }
 
     fn test_input_two(&self) -> &str {
@@ -114,4 +51,46 @@ impl Solution for S {
     fn expected_output_two(&self) -> &str {
         "11387"
     }
+}
+
+fn solve(input: &PuzzleInput, operators: &[char]) -> u64 {
+    input
+        .lines
+        .par_iter()
+        .map(|line| {
+            let mut parts = line.split(':');
+            let test_value = parts.next().unwrap().parse::<u64>().unwrap();
+            let rem = parts
+                .next()
+                .unwrap()
+                .trim()
+                .split(' ')
+                .map(|s| s.parse::<u64>().unwrap())
+                .collect::<Vec<_>>();
+            test_for_solution(operators, &rem, test_value)
+        })
+        .sum()
+}
+
+fn test_for_solution(operators: &[char], rem: &[u64], test_value: u64) -> u64 {
+    for op_combinations in repeat(operators.iter())
+        .take(rem.len() - 1)
+        .multi_cartesian_product()
+    {
+        let mut res = rem[0];
+        for (i, op) in op_combinations.iter().enumerate() {
+            let a = rem[i + 1];
+            match op {
+                '+' => res += a,
+                '*' => res *= a,
+                '|' => res = res * 10u64.pow(a.ilog10() + 1) + a,
+                _ => panic!("Unknown operator"),
+            }
+        }
+
+        if res == test_value {
+            return test_value;
+        }
+    }
+    0
 }
