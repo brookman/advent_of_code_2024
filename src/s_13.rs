7d0dd58b54235ed89b5a40f12484cbc22ec40107
Benjamin Bachmann <beba@Benjamins-MacBook-Pro.local>
2024-12-14 20:42:47 +0100

    Day 13: Refactor

--- src/s_13.rs
+++ src/s_13.rs
@@ -5,7 +5,8 @@ pub struct S;
 use lazy_static::lazy_static;
 use regex::Regex;
 use z3::{
-    ast::{self, Ast},
+    self,
+    ast::{Ast, Int},
     Config, Context, Optimize, SatResult,
 };
 
@@ -16,61 +17,7 @@ lazy_static! {
 
 impl Solution for S {
     fn solve_one(&self, input: &PuzzleInput) -> String {
-        let mut result = 0;
-
-        let mut iter = input.lines.iter();
-        let mut line = iter.next();
-
-        while line.is_some() {
-            let button_a = line.unwrap();
-            line = iter.next();
-            let button_b = line.unwrap();
-            line = iter.next();
-            let prize = line.unwrap();
-            iter.next();
-            line = iter.next();
-
-            let mut ax = 0;
-            let mut ay = 0;
-            let mut bx = 0;
-            let mut by = 0;
-            let mut px = 0;
-            let mut py = 0;
-
-            for mul in BUTTON.captures_iter(button_a) {
-                ax = mul.get(1).unwrap().as_str().parse::<i32>().unwrap();
-                ay = mul.get(2).unwrap().as_str().parse::<i32>().unwrap();
-            }
-
-            for mul in BUTTON.captures_iter(button_b) {
-                bx = mul.get(1).unwrap().as_str().parse::<i32>().unwrap();
-                by = mul.get(2).unwrap().as_str().parse::<i32>().unwrap();
-            }
-
-            for mul in PRIZE.captures_iter(prize) {
-                px = mul.get(1).unwrap().as_str().parse::<i32>().unwrap();
-                py = mul.get(2).unwrap().as_str().parse::<i32>().unwrap();
-            }
-
-            let mut cheapest = i32::MAX;
-
-            for a in 1..=100 {
-                for b in 1..=100 {
-                    let x = ax * a + bx * b;
-                    let y = ay * a + by * b;
-                    if x == px && y == py {
-                        let tokens = a * 3 + b;
-                        cheapest = cheapest.min(tokens);
-                    }
-                }
-            }
-
-            if cheapest != i32::MAX {
-                result += cheapest;
-            }
-        }
-
-        result.to_string()
+        solve(input, 0).to_string()
     }
 
     fn test_input_one(&self) -> &str {
@@ -97,94 +44,7 @@ Prize: X=18641, Y=10279
     }
 
     fn solve_two(&self, input: &PuzzleInput) -> String {
-        let mut result = 0;
-
-        let mut iter = input.lines.iter();
-        let mut line = iter.next();
-
-        while line.is_some() {
-            let button_a = line.unwrap();
-            line = iter.next();
-            let button_b = line.unwrap();
-            line = iter.next();
-            let prize = line.unwrap();
-            iter.next();
-            line = iter.next();
-
-            let mut ax = 0;
-            let mut ay = 0;
-            let mut bx = 0;
-            let mut by = 0;
-            let mut px = 0;
-            let mut py = 0;
-
-            for mul in BUTTON.captures_iter(button_a) {
-                ax = mul.get(1).unwrap().as_str().parse::<i64>().unwrap();
-                ay = mul.get(2).unwrap().as_str().parse::<i64>().unwrap();
-            }
-
-            for mul in BUTTON.captures_iter(button_b) {
-                bx = mul.get(1).unwrap().as_str().parse::<i64>().unwrap();
-                by = mul.get(2).unwrap().as_str().parse::<i64>().unwrap();
-            }
-
-            for mul in PRIZE.captures_iter(prize) {
-                px = mul.get(1).unwrap().as_str().parse::<i64>().unwrap();
-                py = mul.get(2).unwrap().as_str().parse::<i64>().unwrap();
-            }
-
-            let cfg = Config::new();
-            let ctx = Context::new(&cfg);
-
-            let a = ast::Int::new_const(&ctx, "a");
-            let b = ast::Int::new_const(&ctx, "b");
-            let ax = ast::Int::from_i64(&ctx, ax);
-            let ay = ast::Int::from_i64(&ctx, ay);
-            let bx = ast::Int::from_i64(&ctx, bx);
-            let by = ast::Int::from_i64(&ctx, by);
-            let px = ast::Int::from_i64(&ctx, px);
-            let py = ast::Int::from_i64(&ctx, py);
-
-            let c = ast::Int::from_i64(&ctx, 10000000000000);
-            let zero = ast::Int::from_i64(&ctx, 0);
-            let three = ast::Int::from_i64(&ctx, 3);
-
-            let solver = Optimize::new(&ctx);
-            solver.assert(&a.gt(&zero));
-            solver.assert(&b.gt(&zero));
-
-            let eq_1 = ast::Int::add(
-                &ctx,
-                &[
-                    &ast::Int::mul(&ctx, &[&ax, &a]),
-                    &ast::Int::mul(&ctx, &[&bx, &b]),
-                ],
-            );
-            let px = ast::Int::add(&ctx, &[&px, &c]);
-            solver.assert(&eq_1._eq(&px));
-
-            let eq_2 = ast::Int::add(
-                &ctx,
-                &[
-                    &ast::Int::mul(&ctx, &[&ay, &a]),
-                    &ast::Int::mul(&ctx, &[&by, &b]),
-                ],
-            );
-            let py = ast::Int::add(&ctx, &[&py, &c]);
-            solver.assert(&eq_2._eq(&py));
-
-            let to_minimize = ast::Int::add(&ctx, &[&ast::Int::mul(&ctx, &[&a, &three]), &b]);
-            solver.minimize(&to_minimize);
-
-            if solver.check(&[]) == SatResult::Sat {
-                let model = solver.get_model().unwrap();
-                let a = model.eval(&a, true).unwrap().as_i64().unwrap();
-                let b = model.eval(&b, true).unwrap().as_i64().unwrap();
-                result += a * 3 + b;
-            }
-        }
-
-        result.to_string()
+        solve(input, 10000000000000).to_string()
     }
 
     fn test_input_two(&self) -> &str {
@@ -209,4 +69,77 @@ Prize: X=18641, Y=10279
     fn expected_output_two(&self) -> &str {
         "875318608908"
     }
+}
+
+fn solve(input: &PuzzleInput, c: i64) -> i64 {
+    let get_numbers = |regex: &Regex, string: &String| -> (i64, i64) {
+        let cap = regex.captures_iter(string).next().unwrap();
+        (
+            cap.get(1).unwrap().as_str().parse::<i64>().unwrap(),
+            cap.get(2).unwrap().as_str().parse::<i64>().unwrap(),
+        )
+    };
+
+    let mut result = 0;
+
+    let cfg = Config::new();
+    let ctx = &Context::new(&cfg);
+    let a = Int::new_const(ctx, "a");
+    let b = Int::new_const(ctx, "b");
+    let zero = Int::from_i64(ctx, 0);
+    let three = Int::from_i64(ctx, 3);
+    let c = Int::from_i64(ctx, c);
+
+    let mut iter = input.lines.iter();
+    let mut line = iter.next();
+
+    while line.is_some() {
+        let button_a = line.unwrap();
+        line = iter.next();
+        let button_b = line.unwrap();
+        line = iter.next();
+        let prize = line.unwrap();
+        iter.next();
+        line = iter.next();
+
+        let (ax, ay) = get_numbers(&BUTTON, button_a);
+        let (bx, by) = get_numbers(&BUTTON, button_b);
+        let (px, py) = get_numbers(&PRIZE, prize);
+
+        let ax = Int::from_i64(ctx, ax);
+        let ay = Int::from_i64(ctx, ay);
+        let bx = Int::from_i64(ctx, bx);
+        let by = Int::from_i64(ctx, by);
+        let px = Int::from_i64(ctx, px);
+        let py = Int::from_i64(ctx, py);
+
+        let opt = Optimize::new(ctx);
+
+        opt.assert(&a.gt(&zero));
+        opt.assert(&b.gt(&zero));
+        opt.minimize(&Int::add(ctx, &[&Int::mul(ctx, &[&a, &three]), &b]));
+
+        let eq_1 = Int::add(
+            ctx,
+            &[&Int::mul(ctx, &[&ax, &a]), &Int::mul(ctx, &[&bx, &b])],
+        );
+        let px = Int::add(ctx, &[&px, &c]);
+        opt.assert(&eq_1._eq(&px));
+
+        let eq_2 = Int::add(
+            ctx,
+            &[&Int::mul(ctx, &[&ay, &a]), &Int::mul(ctx, &[&by, &b])],
+        );
+        let py = Int::add(ctx, &[&py, &c]);
+        opt.assert(&eq_2._eq(&py));
+
+        if opt.check(&[]) == SatResult::Sat {
+            let model = opt.get_model().unwrap();
+            let a = model.eval(&a, true).unwrap().as_i64().unwrap();
+            let b = model.eval(&b, true).unwrap().as_i64().unwrap();
+            result += a * 3 + b;
+        }
+    }
+
+    result
 }
